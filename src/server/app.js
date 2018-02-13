1bacffc3cf276b361d032a7491f12ac8c49f8f8e
Marcamillian <marcamillian@gmail.com>
2018-02-13 10:37:10 +0000

    Adding .js onto the end of the gameStateManager require statement

--- src/server/app.js
+++ src/server/app.js
@@ -8,7 +8,7 @@ const WebSocket = require('ws');
 const io = require('socket.io')
 const util = require('util')
  
-const GameStateManager = require('./GameStateManager')
+const GameStateManager = require('./GameStateManager.js')
 
 // game objects
 var app = express();  // express app - for handelling routes
