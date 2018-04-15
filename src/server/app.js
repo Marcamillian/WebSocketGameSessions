5fd08cd9799721f4d43a4999ca9abf024a10072b
Marcamillian <marcamillian@gmail.com>
2018-04-15 15:06:56 +0100

    chore: Update the node apps port to take the local environment port
    To make sure that the appropriate port is used for the heroku
    environment - change the port declaration on the express server

--- src/server/app.js
+++ src/server/app.js
@@ -25,6 +25,7 @@ sessionParser = session({
 })
 
 // CONFIGURE EXPRESS APP - serve files from the right folder
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static('./src/client'));
 app.use(sessionParser);
 
@@ -308,4 +309,4 @@ wss.broadcast = (gameRef, gameState = stateManager.getGameState(gameRef), privat
 }
 
 // START THE SERVER
-server.listen(8080, ()=> console.log('Listening on http://localhost:8080'))
+server.listen(app.get('port'), ()=> console.log(`Listening on port ${app.get('port')}`));
